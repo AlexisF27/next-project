bce4eae2ae073d2c36462b9082add1d294dba89f
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2023-12-15 18:41:31 +0000

    Change the size of the Search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,10 +4,10 @@ import { TextField, Box } from "@mui/material";
 
 function Search() {
   return (
-    <Box sx={{ position: 'absolute', top: '100px', left: '650px' }}>
-      <TextField type="search" />
-      <IconButton size="large">
-        <SearchRoundedIcon />
+    <Box sx={{ position: 'absolute', top: '100px', left: '450px' }}>
+      <TextField type="search" style={{width: '600px',paddingRight: '20px'}}/>
+      <IconButton size="large" >
+        <SearchRoundedIcon size="large"  style={{ fontSize: '30px'}}/>
       </IconButton>
     </Box>
   )
